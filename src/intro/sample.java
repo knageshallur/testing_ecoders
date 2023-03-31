9b10b11b28340a83d6cf71b550216769670e658e
K NageshKumar <K NageshKumar@SMTC-R-0200>
2023-03-31 13:22:46 +0530

    opening browser

--- src/intro/sample.java
+++
@@ -1,7 +0,0 @@
-package intro;
-
-public class sample {
-public static void main(String[] args) {
-	System.out.println("demo code pushh");
-}
-}
---
+++ src/opening_browser/TestingEcodersContactpage.java
@@ -0,0 +1,35 @@
+package opening_browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingEcodersContactpage {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\K NageshKumar\\eclipse-workspace\\testing_ecoders\\alljarsfiles\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.navigate().to("http://127.0.0.1:5000/contact");
+		Thread.sleep(1000);
+		String titleofpage=driver.getTitle();
+		System.out.println(titleofpage);
+		String urlofpage=driver.getCurrentUrl();
+		System.out.println(urlofpage);
+		String sourceofpage=driver.getPageSource();
+		System.out.println(sourceofpage);
+		
+		String expectedtitle="ecoders-contact";
+		
+		if(expectedtitle.equals(titleofpage)) {
+			System.out.println("title matched");
+			
+		}
+		else {
+			System.out.println("title not matched");
+		}
+		
+		driver.quit();
+		
+	}
+}
